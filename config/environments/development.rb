0b3960c2fa7a470f13455762895e0ce47db6cfcd
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-01-03 16:41:18 +0800

    changed mailling host to website

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,8 @@ Rails.application.configure do
 
   # Mailer
   config.action_mailer.delivery_method = :smtp #:letter_opener
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "www.joycewong.info" }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
