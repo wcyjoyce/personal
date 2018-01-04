c024ad7f60078fd45ef6860ac76ce729693feb7e
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-01-04 16:03:38 +0800

    CSS for validation error messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,6 +9,7 @@ class MessagesController < ApplicationController
       redirect_to "#contact", notice: "Thanks for leaving a message, I will get back to you shortly!"
     else
       redirect_to "#contact", alert: "There was a problem with your message. Please try again."
+      # render :new, alert: "There was a problem with your message. Please try again."
     end
   end
 
